44266df0c10a593f5d615506e5cc95163cc4d8ac
David Lambl <david.lambl@gmail.com>
2017-09-15 15:23:02 -0400

    - created the "/create-new-device" route for the POC

--- WebServer/src/main/java/Main.java
+++ WebServer/src/main/java/Main.java
@@ -2,6 +2,37 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+    	exception(Exception.class, (e, req, res) -> e.printStackTrace()); // print all exceptions
+        enableCORS();
+    	
+        post("/create-new-device", (request, response) -> {
+        	System.out.println(String.format("Creating Device: %s", request.body()));
+
+        	return String.format("Success! Device Created: %s", request.body());
+        });
+    }
+    
+    private static void enableCORS() {
+    	options("/*",
+	        (request, response) -> {
+	
+	            String accessControlRequestHeaders = request
+	                    .headers("Access-Control-Request-Headers");
+	            if (accessControlRequestHeaders != null) {
+	                response.header("Access-Control-Allow-Headers",
+	                        accessControlRequestHeaders);
+	            }
+	
+	            String accessControlRequestMethod = request
+	                    .headers("Access-Control-Request-Method");
+	            if (accessControlRequestMethod != null) {
+	                response.header("Access-Control-Allow-Methods",
+	                        accessControlRequestMethod);
+	            }
+	
+	            return "OK";
+	        });
+	
+		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
     }
 }
