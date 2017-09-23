c2b73255a66112e1d44152396751950045114711
michaelcantu93 <cantumi@CANTUMI1.americas.hpqcorp.net>
2017-09-22 19:12:12 -0500

    added logger4j for generator

--- PayloadGenerator/src/main/java/com/psu/group1/generator/Payload.java
+++ PayloadGenerator/src/main/java/com/psu/group1/generator/Payload.java
@@ -1,12 +1,20 @@
 package com.psu.group1.generator;
 
-public class Payload implements Runnable {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Payload implements Runnable {
+	
+	private static final Logger logger = LogManager.getLogger();
+	
 	public void run() {
+		logger.debug(">>run()");
 		
 		double temp = ((Math.random() * 20) + 60);
+		logger.info("temp: {}", temp);
 		System.out.println("Sensor value: " + temp + " degrees F");
 		
+		logger.debug("<<run()");
 	}
 
 }
