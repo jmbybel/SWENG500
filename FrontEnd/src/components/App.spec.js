38603f9c98e5812f471625b0a62785b67dd90371
David Lambl <david.lambl@gmail.com>
2017-09-29 13:44:00 -0400

    - more tests for App component

--- FrontEnd/src/components/App.spec.js
+++ FrontEnd/src/components/App.spec.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('<App />', () => {
-  it('render: should have a main menu', () => {
+  it('render: should have a MainMenu component', () => {
     // Arrange, Act
     const result = shallow(<App history={{}} />);
     
@@ -12,6 +12,41 @@ describe('<App />', () => {
     expect(result.node.props.children.length).toEqual(2);
     const mainMenu = result.node.props.children[0];
     expect(mainMenu.type.name).toEqual('MainMenu');
+  });
+
+  it('render: should have a Switch component', () => {
+    // Arrange, Act
+    const result = shallow(<App history={{}} />);
+    
+    // Assert
+    expect(result.node.type).toEqual('section');
+    expect(result.node.props.children.length).toEqual(2);
+    const swch = result.node.props.children[1];
+    expect(swch.type.name).toEqual('Switch');
+  });
+
+  it('render: should have three routes defined', () => {
+    // Arrange, Act
+    const result = shallow(<App history={{}} />);
+    
+    // Assert
+    expect(result.node.type).toEqual('section');
+    expect(result.node.props.children.length).toEqual(2);
+    const swch = result.node.props.children[1];
+    expect(swch.type.name).toEqual('Switch');
+    expect(swch.node.props.children.length).toEqual(3);
+  });
+
+  it('render: should have a route defined for the HomePage component', () => {
+    // Arrange, Act
+    const result = shallow(<App history={{}} />);
+    
+    // Assert
+    expect(result.node.type).toEqual('section');
+    expect(result.node.props.children.length).toEqual(2);
+    const swch = result.node.props.children[1];
+    expect(swch.type.name).toEqual('Switch');
+    expect(swch.node.props.children.length).toEqual(3);
   });
 });
   
