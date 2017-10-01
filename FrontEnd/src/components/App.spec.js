0a529f77b0baaaf2ac1b35d655b8eb7dbc6a3256
David Lambl <dlambl@confluence.com>
2017-10-01 18:10:23 -0400

    - new tests for week 6

--- FrontEnd/src/components/App.spec.js
+++ FrontEnd/src/components/App.spec.js
@@ -6,7 +6,7 @@ describe('<App />', () => {
   it('render: should have a MainMenu component', () => {
     // Arrange, Act
     const result = shallow(<App history={{}} />);
-    
+
     // Assert
     expect(result.node.type).toEqual('section');
     expect(result.node.props.children.length).toEqual(2);
@@ -17,7 +17,7 @@ describe('<App />', () => {
   it('render: should have a Switch component', () => {
     // Arrange, Act
     const result = shallow(<App history={{}} />);
-    
+
     // Assert
     expect(result.node.type).toEqual('section');
     expect(result.node.props.children.length).toEqual(2);
@@ -28,25 +28,60 @@ describe('<App />', () => {
   it('render: should have three routes defined', () => {
     // Arrange, Act
     const result = shallow(<App history={{}} />);
-    
+
     // Assert
     expect(result.node.type).toEqual('section');
     expect(result.node.props.children.length).toEqual(2);
     const swch = result.node.props.children[1];
     expect(swch.type.name).toEqual('Switch');
-    expect(swch.node.props.children.length).toEqual(3);
+    expect(swch.props.children.length).toEqual(3);
   });
 
-  it('render: should have a route defined for the HomePage component', () => {
-    // Arrange, Act
+  it('render: should have a route defined for the HomePage', () => {
+    // Arrange
+    const expected = '/';
+
+    // Act
     const result = shallow(<App history={{}} />);
-    
+
     // Assert
     expect(result.node.type).toEqual('section');
     expect(result.node.props.children.length).toEqual(2);
     const swch = result.node.props.children[1];
     expect(swch.type.name).toEqual('Switch');
-    expect(swch.node.props.children.length).toEqual(3);
+    const homePage = swch.props.children[0];
+    expect(homePage.props.path).toEqual(expected);
+  });
+
+  it('render: should have a route defined for the NewDevicePage', () => {
+    // Arrange
+    const expected = '/create-new-device';
+
+    // Act
+    const result = shallow(<App history={{}} />);
+
+    // Assert
+    expect(result.node.type).toEqual('section');
+    expect(result.node.props.children.length).toEqual(2);
+    const swch = result.node.props.children[1];
+    expect(swch.type.name).toEqual('Switch');
+    const newDevicePage = swch.props.children[1];
+    expect(newDevicePage.props.path).toEqual(expected);
+  });
+
+  it('render: should have a route defined for the DevicesPage', () => {
+    // Arrange
+    const expected = '/view-devices';
+
+    // Act
+    const result = shallow(<App history={{}} />);
+
+    // Assert
+    expect(result.node.type).toEqual('section');
+    expect(result.node.props.children.length).toEqual(2);
+    const swch = result.node.props.children[1];
+    expect(swch.type.name).toEqual('Switch');
+    const viewDevices = swch.props.children[2];
+    expect(viewDevices.props.path).toEqual(expected);
   });
 });
-  
