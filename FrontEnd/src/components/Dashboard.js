278a733960c705206a775ce722513d38de13838d
michaelcantu93 <michaelcantu93@gmail.com>
2017-10-24 15:09:48 -0500

    added favicon

--- FrontEnd/src/components/Dashboard.js
+++ FrontEnd/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { PageHeader } from 'react-bootstrap';
 const Dashboard = () => {
   return (
     <section>
-      <PageHeader>IoT Data Generator</PageHeader>
+      <PageHeader>Mock IoT Data Generator Project</PageHeader>
       <h2>Click on Create New Sensor</h2>
     </section>
   );
