bd06d26168f30588cf954c7d540381e2d3c3d620
Team1 <Team1@SWENG500-Team1.SWENG500-Team1.b6.internal.cloudapp.net>
2017-10-25 23:14:32 +0000

    updating to use port 80

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', 80);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/dist/index.html');
@@ -12,20 +12,20 @@ app.get('/favicon.ico', function(req, res){
   res.sendFile(__dirname + '/dist/favicon.ico');
 });
 
-app.get('/main.50ee8b3c63b608a07e75.js', function(req, res){
-  res.sendFile(__dirname + '/dist/main.50ee8b3c63b608a07e75.js');
+app.get('/main.e2969e325f8d68684fcb.js', function(req, res){
+  res.sendFile(__dirname + '/dist/main.e2969e325f8d68684fcb.js');
 });
 
-app.get('/main.50ee8b3c63b608a07e75.js.map', function(req, res){
-  res.sendFile(__dirname + '/dist/main.50ee8b3c63b608a07e75.js.map');
+app.get('/main.e2969e325f8d68684fcb.js.map', function(req, res){
+  res.sendFile(__dirname + '/dist/main.e2969e325f8d68684fcb.js.map');
+});
+
+app.get('/main.adca5dee1ad7cc6b408ab11982cee251.css.map.', function(req, res){
+  res.sendFile(__dirname + '/dist/main.adca5dee1ad7cc6b408ab11982cee251.css.map');
 });
 
 app.get('/main.adca5dee1ad7cc6b408ab11982cee251.css', function(req, res){
   res.sendFile(__dirname + '/dist/main.adca5dee1ad7cc6b408ab11982cee251.css');
-});
-
-app.get('/main.adca5dee1ad7cc6b408ab11982cee251.css.map', function(req, res){
-  res.sendFile(__dirname + '/dist/main.adca5dee1ad7cc6b408ab11982cee251.css.map');
 });
 
 // Listen for requests
--- WebServer/src/main/java/edu/psu/iot/webserver/Main.java
+++ WebServer/src/main/java/edu/psu/iot/webserver/Main.java
@@ -14,6 +14,7 @@ import edu.psu.iot.generator.sensor.SensorService;
 
 public class Main {
     public static void main(String[] args) {
+    	port(4000);
     	configureExceptionHandling();
         enableCORS();
 
