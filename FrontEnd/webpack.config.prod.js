de9a50e27b3d9c5c93f9cfdaeff7705b856a06db
David Lambl <david.lambl@gmail.com>
2017-10-27 12:25:48 -0400

    removing chunkhash from frontend dist files for now

--- FrontEnd/webpack.config.prod.js
+++ FrontEnd/webpack.config.prod.js
@@ -22,7 +22,7 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].[chunkhash].js'
+    filename: '[name].js'
   },
   plugins: [
     // Hash the files using MD5 so that their names change when the content changes.
@@ -32,7 +32,7 @@ export default {
     new webpack.DefinePlugin(GLOBALS),
 
     // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[contenthash].css'),
+    new ExtractTextPlugin('[name].css'),
 
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
